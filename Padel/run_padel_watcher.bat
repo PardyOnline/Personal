d2ad9a74f987511826b64580e479e0926260743a
Sean Pardy <113913622+PardyOnline@users.noreply.github.com>
2025-11-12 23:14:05 +0000

    Update and rename run_padel_watcher.bat to Padel/run_padel_watcher.bat

--- Padel/run_padel_watcher.bat
+++ Padel/run_padel_watcher.bat
@@ -1,3 +1,4 @@
 @echo off
-cd /d C:\Users\Me\Documents\padel_scraper
+cd ***Enter Directory of padel_watcher file***
 "C:\Users\Me\Documents\padel_scraper\.venv\Scripts\python.exe" padel_watcher.py
+
